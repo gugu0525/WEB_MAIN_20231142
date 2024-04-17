19acf4f4834f8745dca7d471d15b7c7349902f22
admin <eunso3378@naver.com>
2024-04-17 15:15:03 +0900

    1

--- js/test.js
+++ js/test.js
@@ -21,6 +21,15 @@ console.log(e);
 
 document.getElementById("search_btn").addEventListener('click', search_message);
 
-function search_message(){
+// function search_message(){
     alert("검색을 수행합니다!");
-}
+// }
+
+const over = (obj) => {
+    obj.src = "image/H-logo.png";
+};
+
+const search_message = () => {
+    const c = '검색을 수행합니다';
+    alert(c);
+};
