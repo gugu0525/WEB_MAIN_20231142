32e09a4f3816ae88eab3ae64fdb75c2194dd638b
admin <eunso3378@naver.com>
2024-04-24 15:21:12 +0900

    111

--- js/login.js
+++ js/login.js
@@ -1,9 +1,25 @@
-const check_input = () => {
-    const loginForm = document.getElementById('login_form');
-    const loginBtn = document.getElementById('login_btn');
-    const emailInput= document.getElementById('typeEmailX');
-    const passwordInput = document.getElementById('typePasswordX');
 
+    if (emailValue.length < 5) {
+    alert('아이디는최소5글자이상입력해야합니다.');
+    return false;
+    }
+
+    if (passwordValue.length < 12) {
+        alert('비밀번호는반드시12글자이상입력해야합니다.');
+        return false;
+        }
+    const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?]+/) !== null;
+    if (!hasSpecialChar) {
+        alert('패스워드는특수문자를1개이상포함해야합니다.');
+        return false;
+        }
+    const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
+    const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
+    if (!hasUpperCase || !hasLowerCase) {
+        alert('패스워드는대소문자를1개이상포함해야합니다.');
+        return false;
+    }
+       
     const c = '아이디, 패스워드를 체크합니다';
     alert(c);
 
