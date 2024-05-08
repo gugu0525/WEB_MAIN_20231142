f75b67fabdfa88026ce41468b5994e3b9c8e8fb9
admin <eunso3378@naver.com>
2024-05-08 15:39:33 +0900

    333

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,7 +1,7 @@
 function pop_up() {
     var cookieCheck = getCookie("popupYN");
     if (cookieCheck != "N"){
-    window.open("../popup/pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 }
 
