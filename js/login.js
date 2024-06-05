10ec7f017a46beb2856527e9f3067fae2b2a07d8
eunso3378 <eunso3378@naver.com>
2024-06-05 15:07:08 +0900

    11

--- js/login.js
+++ js/login.js
@@ -123,45 +123,6 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
 
 
 
-function encodeByAES256(key, data)
-    {const cipher= CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
-    iv: CryptoJS.enc.Utf8.parse(""),
-    padding: CryptoJS.pad.Pkcs7,
-    mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString();
-}
-
-
-function decodeByAES256(key, data)
-    {const cipher= CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
-    iv: CryptoJS.enc.Utf8.parse(""),
-    padding: CryptoJS.pad.Pkcs7,
-    mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString(CryptoJS.enc.Utf8);
-    }
-
-
-function encrypt_text(password){
-    const k = "key"; // 클라이언트 키
-   const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const b = password;
-    const eb = this.encodeByAES256(rk, b);
-    return eb;
-    console.log(eb);
-}
-
-
-
-function decrypt_text(){
-    const k = "key"; // 서버의 키
-   const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const eb = session_get();
-    const b = this.decodeByAES256(rk, eb);
-    console.log(b); 
-}
-
 
 
 
