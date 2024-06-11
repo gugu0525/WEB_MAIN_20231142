5ba8469e3422cd28f7541891996911bdf0239a12
eunso3378 <eunso3378@naver.com>
2024-06-12 01:25:25 +0900

    1

--- js/join.js
+++ js/join.js
@@ -1,5 +1,46 @@
 
 
+class SignUp {
+    constructor(firstName, lastName, birthdayDate, gender, emailAddress, phoneNumber, classNumber, random) { // 생성자 함수
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.birthdayDate = birthdayDate;
+      this.gender = gender;
+      this.emailAddress = emailAddress;
+      this.phoneNumber = phoneNumber;
+      this.classNumber = classNumber;
+      this.random = random;
+    }
+  
+  // 다음 페이지 소스 코드 이어서 작성
+
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`; // 템플릿 리터럴 문자열 연결, 기존에는 + 연산자로 연결
+  }
+
+  set fullName(fullName) {
+    const [firstName, lastName] = fullName.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get contactInfo() {
+    return `${this.emailAddress} ${this.phoneNumber} ${this.random}`; // 요소 하나 하나를 객체 프로퍼티라고 한다.
+  }
+
+  set contactInfo(contactInfo) {
+    const [emailAddress, phoneNumber, random] = contactInfo.split(" ");
+    this.emailAddress = emailAddress;
+    this.phoneNumber = phoneNumber;
+    this.random = random;
+      
+  }
+}
+
+
+
+
 function join(){ // 회원가입
         let form = document.querySelector("#form_main");
         let f_name = document.querySelector("#firstName");
